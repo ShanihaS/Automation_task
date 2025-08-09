fca5c1093e16c1e312d5756e3bc4ebb84d9389f3
ShanihaS <146363150+ShanihaS@users.noreply.github.com>
2025-08-09 20:04:26 +0530

    Update Browser.java

--- Browser.java
+++ Browser.java
@@ -3,6 +3,12 @@ package week1.day2;
 public class Browser {
 	public static void main (String [] args) {
 		System.out.println("This is my Browser");
+
+        Chrome obj = new Chrome();
+		obj.getName();
+		obj.printName();	
+		
 	}
  
 }
+
